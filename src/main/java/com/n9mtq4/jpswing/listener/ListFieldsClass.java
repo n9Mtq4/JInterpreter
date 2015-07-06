269bb170532450d0c9609c8523946a6a4e0d5ea6
n9Mtq4 <bill.wresnahan@gmail.com>
2015-07-06 12:08:34 -0400

    listing fields

---
+++ src/main/java/com/n9mtq4/jpswing/listener/ListFieldsClass.java
@@ -0,0 +1,40 @@
+package com.n9mtq4.jpswing.listener;
+
+import com.n9mtq4.console.lib.BaseConsole;
+import com.n9mtq4.console.lib.ConsoleListener;
+import com.n9mtq4.console.lib.events.ConsoleActionEvent;
+import com.n9mtq4.reflection.ReflectionHelper;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+
+/**
+ * Created by will on 7/6/15 at 2:01 AM.
+ */
+public class ListFieldsClass extends ConsoleListener {
+	
+	/**
+	 * 0 = listfields
+	 * 1 = class name
+	 * */
+//	TODO: BAD - figure out a way to tell if class or variable, so it can print a failed message
+	@Override
+	public void actionPerformed(ConsoleActionEvent consoleActionEvent, BaseConsole baseConsole) {
+		
+		if (!consoleActionEvent.getCommand().getArg(0).equalsIgnoreCase("listfields")) return;
+		if (consoleActionEvent.getCommand().getLength() != 2) return;
+		
+		try {
+			
+			Class clazz = ReflectionHelper.getClass(consoleActionEvent.getCommand().getArg(1));
+			Field[] fields = ReflectionHelper.getAllDeclaredFields(clazz);
+			
+			baseConsole.println(Arrays.toString(fields));
+			
+		}catch (Exception e) {
+			
+		}
+		
+	}
+	
+}
